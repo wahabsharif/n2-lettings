c7f7559a81d79b7088921c1594d79e30c224a6d8
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-08 12:28:02 +0500

    Dual Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          {children}
+        </main>
       </body>
     </html>
   );
