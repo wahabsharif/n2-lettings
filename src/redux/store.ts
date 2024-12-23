00ee776799f832a90e6b02e4b1148c4768d60d1a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-12-24 00:09:31 +0500

    redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice";
+import authReducer from "@/redux/slices/authSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
-  devTools: true,
 });
 
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
